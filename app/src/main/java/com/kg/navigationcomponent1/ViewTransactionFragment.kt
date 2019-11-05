126f632b33c79b2754cfb3249341ca6c5c99d534
trbibi <trbilgisayarbilimleri@gmail.com>
2019-11-06 01:11:53 +0300

    navigation between fragments

---
+++ app/src/main/java/com/kg/navigationcomponent1/ViewTransactionFragment.kt
@@ -0,0 +1,24 @@
+package com.kg.navigationcomponent1
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ViewTransactionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_view_transaction, container, false)
+    }
+}
